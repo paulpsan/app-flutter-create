e63994b72c5ce9b18bc6b1757de0004ed3a0eea6
paul sanchez <paulpsan@gmail.com>
2019-04-01 07:23:07 -0400

    add buttons

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget{
           leading: Icon(Icons.arrow_back),
           title: Text("Tic Tac Toe"),
         ),
-        body: Text(
+/*        body: Text(
           'Hola, Mundo ! Lorem ipsum',
           textDirection: TextDirection.ltr,
           style: TextStyle(
@@ -23,11 +23,70 @@ class MyApp extends StatelessWidget{
             fontWeight: FontWeight.bold,
           ),
 
+        ),*/
+
+        body: Stack(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                RowButtonAction(),
+                RowButtonAction(),
+                RowButtonAction(),
+              ],
+            )
+          ],
         ),
+
       ),
     );
   }
 
 }
 
-class ActionButton
+class RowButtonAction extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Row(
+      children: <Widget>[
+        ActionButtonStarComputer(),
+        ActionButtonStarComputer(),
+        ActionButtonStarComputer(),
+      ],
+    );
+  }
+
+}
+
+class ActionButtonStarComputer extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+
+    return _ActionButtonStarComputer();
+  }
+
+}
+class _ActionButtonStarComputer extends State<ActionButtonStarComputer>{
+  bool sw = false;
+  void onPressedButton(){
+    setState(() {
+      sw=!this.sw;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return  FloatingActionButton(
+      child: Icon(sw?Icons.favorite_border:Icons.favorite),
+      backgroundColor: Colors.green,
+      mini: true,
+      onPressed: onPressedButton,
+    );
+  }
+}
+// implementacion de BottomNavigatorBar
+
+
