f222b414d21f8be9f992594a301a4c89189bf9d9
paul sanchez <paulpsan@gmail.com>
2019-04-07 23:12:36 -0400

    add turn

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 var imgX = "assets/images/X.jpg";
 var imgO = "assets/images/O.jpg";
+var imgI = "assets/images/init.jpg";
 var turn = imgX;
 var aG = [
   ["", "", ""],
@@ -32,27 +33,13 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// class RBA extends StatelessWidget {
-//   int _row;
-//   RBA(this._row);
-//   @override
-//   Widget build(BuildContext context) {
-//     return Row(
-//       mainAxisAlignment: MainAxisAlignment.center,
-//       children: <Widget>[
-//         ABC(0, _row, true, init),
-//         ABC(1, _row, true, init),
-//         ABC(2, _row, true, init),
-//       ],
-//     );
-//   }
-// }
 class BodyWidget extends StatefulWidget {
   _BodyWidgetState createState() => _BodyWidgetState();
 }
 
 class _BodyWidgetState extends State<BodyWidget> {
   String _icon = "assets/images/init.jpg";
+  String _turn = imgX;
   bool _active = false;
   void _handleTapboxChanged() {
     setState(() {
@@ -66,6 +53,19 @@ class _BodyWidgetState extends State<BodyWidget> {
     });
   }
 
+  void _handleTurnChanged(String value) {
+    setState(() {
+      print(turn);
+      _turn = turn;
+    });
+  }
+
+  void _onWinner(String value) {
+    setState(() {
+      print(value);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -74,32 +74,34 @@ class _BodyWidgetState extends State<BodyWidget> {
         Center(
           child: Text('Tic Tac Toe!'),
         ),
-        ABC(4, 4, false, turn),
-        new Column(
+        GestureDetector(
+          child: ABC(0, 0, false, _turn, _handleTurnChanged, _onWinner),
+        ),
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                ABC(0, 0, true, _icon),
-                ABC(1, 0, true, _icon),
-                ABC(2, 0, true, _icon),
+                ABC(0, 0, true, _icon, _handleTurnChanged, _onWinner),
+                ABC(1, 0, true, _icon, _handleTurnChanged, _onWinner),
+                ABC(2, 0, true, _icon, _handleTurnChanged, _onWinner),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                ABC(0, 1, true, _icon),
-                ABC(1, 1, true, _icon),
-                ABC(2, 1, true, _icon),
+                ABC(0, 1, true, _icon, _handleTurnChanged, _onWinner),
+                ABC(1, 1, true, _icon, _handleTurnChanged, _onWinner),
+                ABC(2, 1, true, _icon, _handleTurnChanged, _onWinner),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                ABC(0, 2, true, _icon),
-                ABC(1, 2, true, _icon),
-                ABC(2, 2, true, _icon),
+                ABC(0, 2, true, _icon, _handleTurnChanged, _onWinner),
+                ABC(1, 2, true, _icon, _handleTurnChanged, _onWinner),
+                ABC(2, 2, true, _icon, _handleTurnChanged, _onWinner),
               ],
             )
           ],
@@ -116,7 +118,10 @@ class ABC extends StatefulWidget {
   bool _state;
   String _icon;
   int _col, _row;
-  ABC(this._col, this._row, this._state, this._icon);
+  ValueChanged<String> changeTurn;
+  ValueChanged<String> winner;
+  ABC(this._col, this._row, this._state, this._icon, this.changeTurn,
+      this.winner);
   @override
   State<StatefulWidget> createState() {
     return _ABC();
@@ -125,46 +130,27 @@ class ABC extends StatefulWidget {
 
 class _ABC extends State<ABC> {
   bool sw = false;
+
   checkWinner(aG) {
-    var result = null;
+    String result = null;
     var sum = aG[0][0] + aG[1][1] + aG[2][2];
     if (sum == "XXX" || sum == "OOO") {
-      result = {
-        "win": sum == "XXX" ? "X" : "O",
-        "row": 0,
-        "column": 0,
-        "diago": 1
-      };
+      result = sum == "XXX" ? "X" : "O";
     }
     sum = aG[0][2] + aG[1][1] + aG[2][0];
     if (sum == "XXX" || sum == "OOO") {
-      result = {
-        "win": sum == "XXX" ? "X" : "O",
-        "row": 0,
-        "column": 0,
-        "diago": 2
-      };
+      result = sum == "XXX" ? "X" : "O";
     }
     if (result == null) {
       for (var i = 0; i < 3; i++) {
         var sum = aG[i][0] + aG[i][1] + aG[i][2];
         if (sum == "XXX" || sum == "OOO") {
-          result = {
-            "win": sum == "XXX" ? "X" : "O",
-            "row": i + 1,
-            "column": 0,
-            "diago": 0
-          };
+          result = sum == "XXX" ? "X" : "O";
           break;
         }
         sum = aG[0][i] + aG[1][i] + aG[2][i];
         if (sum == "XXX" || sum == "OOO") {
-          result = {
-            "win": sum == "XXX" ? "X" : "O",
-            "row": 0,
-            "column": i + 1,
-            "diago": 0
-          };
+          result = sum == "XXX" ? "X" : "O";
           break;
         }
       }
@@ -175,17 +161,18 @@ class _ABC extends State<ABC> {
   void onPressedButton() {
     setState(() {
       if (widget._state) {
-        print(widget._icon);
         if (aG[widget._row][widget._col] == "") {
           widget._icon = turn;
           aG[widget._row][widget._col] = turn == imgX ? "X" : "O";
           turn = turn == imgX ? imgO : imgX;
+          widget.changeTurn(turn);
         }
       }
       print(aG);
       var res = checkWinner(aG);
       if (res != null) {
         print("object $res");
+        widget.winner(turn);
       }
     });
   }
@@ -202,7 +189,12 @@ class _ABC extends State<ABC> {
         child: Container(
           decoration: new BoxDecoration(
               image: DecorationImage(
-                  image: AssetImage(widget._icon), fit: BoxFit.cover)),
+                  image: AssetImage(widget._state
+                      ? (aG[widget._row][widget._col] == ""
+                          ? imgI
+                          : aG[widget._row][widget._col] == "X" ? imgX : imgO)
+                      : widget._icon),
+                  fit: BoxFit.cover)),
         ),
         onPressed: onPressedButton,
       ),
