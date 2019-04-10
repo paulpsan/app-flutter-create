c33c81f74894c6572ccaf535d2ad318c71592afc
Paul Sanchez <psanchez@aj.gob.bo>
2019-04-10 09:06:58 -0400

    add Timer winner!!

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 void main() => runApp(MyApp());
 var imgX = "assets/images/X.jpg",imgO = "assets/images/O.jpg",imgI = "assets/images/init.jpg",turn = imgX,aG = [["", "", ""],["", "", ""],["", "", ""]];
 class MyApp extends StatelessWidget {
@@ -19,7 +20,7 @@ class BodyWidget extends StatefulWidget {
   _BodyWidgetState createState() => _BodyWidgetState();
 }
 class _BodyWidgetState extends State<BodyWidget> {
-  String _icon = imgI,_turn = imgX;
+  String _icon = imgI,_turn = imgX; Timer _timer;
   bool _win=false, _active = false;
   final _style = TextStyle(color: Colors.blueAccent, fontSize: 40);
   void _oNG() {
@@ -33,9 +34,11 @@ class _BodyWidgetState extends State<BodyWidget> {
     });
   }
   void _oW(String value) {
-    setState(() {
-      if (value != null) 
-        _win = true; _turn = value == "X" ? imgX : imgO;
+    _timer = new Timer(const Duration(milliseconds: 1000), () {
+      setState(() {
+        if (value != null) 
+          _win = true; _turn = value == "X" ? imgX : imgO;
+      });
     });
   }
 @override
@@ -43,8 +46,8 @@ class _BodyWidgetState extends State<BodyWidget> {
     return !_win? Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
-              Center(child: Text('Tic Tac Toe!', style: _style),),
-              GestureDetector(child: ABC(0, 0, false, _turn, _hTC, _oW),),
+              Center(child: Text('Tic Tac Toe!', style: _style)),
+              Column(children: <Widget> [Text('El Turno es de :', style: TextStyle(color: Colors.redAccent, fontSize: 25)),ABC(0, 0, false, _turn, _hTC, _oW)]),
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
@@ -54,29 +57,21 @@ class _BodyWidgetState extends State<BodyWidget> {
                     children: <Widget>[ABC(0, 1, true, _icon, _hTC, _oW),ABC(1, 1, true, _icon, _hTC, _oW),ABC(2, 1, true, _icon, _hTC, _oW),],),
                   Row(mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[ABC(0, 2, true, _icon, _hTC, _oW),ABC(1, 2, true, _icon, _hTC, _oW),ABC(2, 2, true, _icon, _hTC, _oW),],)
-                ],
-              ),
+                ]),
               GestureDetector(onTap: _oNG,child: RaisedButton(child: Text('New Game', style: _style))),
-            ],
-          )
+            ])
         : Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
-              Center(child: Text('El Ganador Es :'),),
+              Center(child: Text('El Ganador Es :',style: _style)),
               ABC(0, 0, false, _turn, _hTC, _oW),
-              GestureDetector(
-                  onTap: _oNG,
-                  child: RaisedButton(
-                      child: Text('New Game',style: _style,))),
-            ],
-          );
+              GestureDetector(onTap: _oNG,child: RaisedButton(child: Text('New Game',style: _style))),
+            ]);
   }
 }
 
 class ABC extends StatefulWidget {
-  bool _state;
-  String _icon;
-  int _c, _r;
+  bool _state;String _icon;int _c, _r;
   ValueChanged<String> cTurn,winner;
   ABC(this._c, this._r, this._state, this._icon, this.cTurn,this.winner);
   @override
@@ -87,8 +82,7 @@ class ABC extends StatefulWidget {
 
 class _ABC extends State<ABC> {
   cWin(aG) {
-    String result = null;
-    var sum = aG[0][0] + aG[1][1] + aG[2][2];
+    String result = null;var sum = aG[0][0] + aG[1][1] + aG[2][2];
     if (sum == "XXX" || sum == "OOO") 
       result = sum == "XXX" ? "X" : "O";
     sum = aG[0][2] + aG[1][1] + aG[2][0];
@@ -112,11 +106,10 @@ class _ABC extends State<ABC> {
   }
   void oPB() {
     setState(() {
-      if (widget._state) {
+      if (widget._state) 
         if (aG[widget._r][widget._c] == "") {
           widget._icon = turn; aG[widget._r][widget._c] = turn == imgX ? "X" : "O"; turn = turn == imgX ? imgO : imgX; widget.cTurn(turn);
         }
-      }
       cWin(aG);
     });
   }
@@ -135,7 +128,6 @@ class _ABC extends State<ABC> {
                   image: AssetImage(widget._state? (aG[widget._r][widget._c] == ""? imgI: aG[widget._r][widget._c] == "X" ? imgX : imgO): widget._icon),fit: BoxFit.cover)),
         ),
         onPressed: oPB,
-      ),
-    );
+      ));
   }
 }
